a6700bfca8cefc3629de29e14f84769951d5af2a
Charlotte <charlotte.saadaoui@oteria.fr>
2026-09-22 11:57:47 +0200

    added file_checker function

--- main.c
+++ main.c
@@ -3,23 +3,18 @@
 #include <sys/stat.h>
 #include <string.h>
 
-int file_checker(const char *file){
-	return access(file, F_OK) == 0;
-}
-
-int filecheck(char *file) {
-	return 0;
+int file_checker(char *file) {
+	struct stat st;
+	return stat(file, &st) == 0 && S_ISREG(st.st_mode);
 }
 
 int main(int argc, char **argv) {
 	int i = 0;
-	if (argc > 1) {
-		int len = strlen(argv[1]);
+	if (argc == 1) {
 		write(1, "ok\n", 3);
-		printf("%d", file_checker(argv[len]));
-
+		printf("%d", file_checker(argv[1]));
 	} else {
-		write(1, "please enter arguments to use the command\n", 42);
+		write(1, "please enter one arguments to use the command\n", 42);
 	}
 	return 0;
 }

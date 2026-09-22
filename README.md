4496d6679e13f16134571fe766caec50d06f414b
Charlotte <charlotte.saadaoui@oteria.fr>
2026-09-22 14:40:56 +0200

    added: logic structure compilation, execution, cleaning of a c source file

---
+++ README.md
@@ -0,0 +1,13 @@
+# CRun a C runner for quick tests
+
+usage exemple : 
+```crun main.c``` 
+
+intended execution :
+
+```crun execution -> gcc compilation -> code execution -> compiled files deletion```
+
+### Why this project ? 
+
+Im too lazy to run 3 commands when i test my code, so i made this.
+(thats not really what the lazyness is)
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <unistd.h> // access
+#include <unistd.h> 
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <string.h>
 
 int file_checker(char *file) {
@@ -8,13 +9,49 @@ int file_checker(char *file) {
 	return stat(file, &st) == 0 && S_ISREG(st.st_mode);
 }
 
+int file_extension_checker(const char *file) {
+    const char *extension = strrchr(file, '.');
+
+    return extension != NULL &&
+           (strcmp(extension, ".c") == 0 ||
+            strcmp(extension, ".h") == 0);
+}
+
+void compile(const char *file) {
+	char command[256];
+	snprintf(command, sizeof(command), "gcc %s -o output", file);
+	system(command);
+}
+
+void run_output(void) {
+	system("./output");
+}
+
+void clean_output(void) {
+	system("rm -f output");
+}
+
+void help(void) {
+	printf("Usage: command <file>\n");
+	printf("Compile, execute and clean the output of a C source file.\n");
+}
+
 int main(int argc, char **argv) {
 	int i = 0;
-	if (argc == 1) {
-		write(1, "ok\n", 3);
-		printf("%d", file_checker(argv[1]));
+	if (argc == 2) {
+		if (argv[1] && strcmp(argv[1], "-h") == 0) {
+			help();
+		} else {
+			if (file_checker(argv[1]) == 1 && file_extension_checker(argv[1]) == 1) {
+				compile(argv[1]);
+				run_output();
+				clean_output();
+			} else {
+				printf("File is not a valid C source or header file or does not exist.\n");
+		}
+		}
 	} else {
-		write(1, "please enter one arguments to use the command\n", 42);
+		printf("please enter one argument to use the command\n");
 	}
 	return 0;
 }
